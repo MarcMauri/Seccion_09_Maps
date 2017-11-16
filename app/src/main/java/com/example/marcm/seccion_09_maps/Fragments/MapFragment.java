446ccfdfc02d296ce4384fabc9804a538950da43
marcc992 <marcmauri92@gmail.com>
2017-11-16 17:43:16 +0100

    Puntito azul para ir a nuestra ubicacion

--- app/src/main/java/com/example/marcm/seccion_09_maps/Fragments/MapFragment.java
+++ app/src/main/java/com/example/marcm/seccion_09_maps/Fragments/MapFragment.java
@@ -1,45 +1,32 @@
 package com.example.marcm.seccion_09_maps.Fragments;
 
 
+import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.location.Address;
-import android.location.Geocoder;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.marcm.seccion_09_maps.R;
-import com.google.android.gms.maps.CameraUpdate;
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.Locale;
-
-public class MapFragment extends Fragment implements OnMapReadyCallback, GoogleMap.OnMarkerDragListener, View.OnClickListener {
+public class MapFragment extends Fragment implements OnMapReadyCallback, View.OnClickListener {
 
     private View rootView;
     private GoogleMap gMap;
     private MapView mapView;
-
-    private Geocoder geocoder;
-    private List<Address> addresses;
-
-    private MarkerOptions marker;
 
     private FloatingActionButton fab;
 
@@ -73,15 +60,29 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, GoogleM
         }
     }
 
-    private void checkIfGpsIsEnabled() {
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        gMap = googleMap;
+
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) return;
+        gMap.setMyLocationEnabled(true);
+
+        gMap.setOnMyLocationButtonClickListener(new GoogleMap.OnMyLocationButtonClickListener() {
+            @Override
+            public boolean onMyLocationButtonClick() {
+                Toast.makeText(getContext(), "Hola!", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+    }
+
+    private boolean isGpsEnabled() {
         try {
             int gpsSignal = Settings.Secure.getInt(getActivity().getContentResolver(), Settings.Secure.LOCATION_MODE);
-
-            if (gpsSignal == GPS_DISABLED) {
-                showInfoAlert();
-            }
+            return gpsSignal == GPS_ENABLED;
         } catch (Settings.SettingNotFoundException e) {
             e.printStackTrace();
+            return false;
         }
     }
 
@@ -101,72 +102,9 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, GoogleM
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        gMap = googleMap;
-
-        /* Create marker */
-        LatLng place = new LatLng(37.3890924, -5.9844589);
-        /* Set camera update: zoom */
-        CameraUpdate zoom = CameraUpdateFactory.zoomTo(15);
-
-        // Customizando el marker
-        marker = new MarkerOptions();
-        marker.position(place);
-        marker.title("Mi marcador");
-        marker.draggable(true);
-        marker.snippet("Esto es una caja de texto donde modificar los datos");
-        marker.icon(BitmapDescriptorFactory.fromResource(android.R.drawable.star_on));
-
-        /* Add marker on map */
-        gMap.addMarker(marker);
-        /* Set camera position to added marker */
-        gMap.moveCamera(CameraUpdateFactory.newLatLng(place));
-        /* Set camera animation: zoom */
-        gMap.animateCamera(zoom);
-        ;
-
-        /* Set OnMarkerDrag Listener*/
-        gMap.setOnMarkerDragListener(this);
-
-        /* Este objeto nos permite obtener los datos del mapa tales como calles, paises,...) */
-        geocoder = new Geocoder(getContext(), Locale.getDefault());
-    }
-
-    @Override
-    public void onMarkerDragStart(Marker marker) {
-        marker.hideInfoWindow();
-    }
-
-    @Override
-    public void onMarkerDrag(Marker marker) {
-
-    }
-
-    @Override
-    public void onMarkerDragEnd(Marker marker) {
-        double latitude = marker.getPosition().latitude;
-        double longitude = marker.getPosition().longitude;
-
-        try {
-            addresses = geocoder.getFromLocation(latitude, longitude, 1);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        Address currentAddress = addresses.get(0);
-        String address = currentAddress.getAddressLine(0);
-        String city = currentAddress.getLocality();
-        String state = currentAddress.getAdminArea();
-        String country = currentAddress.getCountryName();
-        String postalCode = currentAddress.getPostalCode();
-
-        marker.setSnippet(city + ",  " + country + " (" + postalCode + ")");
-        marker.showInfoWindow();
-
-    }
-
-    @Override
     public void onClick(View view) {
-        this.checkIfGpsIsEnabled();
+        if (!this.isGpsEnabled()) {
+            this.showInfoAlert();
+        }
     }
 }
